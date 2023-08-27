1091f92edfad033b8c0677596c2e2a758fbc9f7d
Minoworu <zeyrek.mino@hotmail.com>
2023-08-27 15:06:29 +0200

    Changed clas from AStar to FlowField

--- FLOWFIELD/FLOWFIELD/source/framework/EliteAI/EliteGraphs/EliteGraphAlgorithms/EFlowfield.h
+++ FLOWFIELD/FLOWFIELD/source/framework/EliteAI/EliteGraphs/EliteGraphAlgorithms/EFlowfield.h
@@ -1,5 +1,56 @@
 #pragma once
-class EFlowfield
+#include "framework/EliteAI/EliteNavigation/ENavigation.h"
+#include "projects/Movement/SteeringBehaviors/SteeringAgent.h"
+#include <vector>
+namespace Elite
 {
-};
+	template <class T_NodeType, class T_ConnectionType>
+	class Flowfield
+	{
+	public:
+		Flowfield(IGraph<T_NodeType, T_ConnectionType>* pGraph, Heuristic hFunction);
 
+		// stores the optimal connection to a node and its total costs related to the start and end node of the path
+		struct NodeRecord
+		{
+			T_NodeType* pNode = nullptr;
+			T_ConnectionType* pConnection = nullptr;
+			float costSoFar = 0.f; // accumulated g-costs of all the connections leading up to this one
+			float estimatedTotalCost = 0.f; // f-cost (= costSoFar + h-cost)
+
+			bool operator==(const NodeRecord& other) const
+			{
+				return pNode == other.pNode
+					&& pConnection == other.pConnection
+					&& costSoFar == other.costSoFar
+					&& estimatedTotalCost == other.estimatedTotalCost;
+			};
+
+			bool operator<(const NodeRecord& other) const
+			{
+				return estimatedTotalCost < other.estimatedTotalCost;
+			};
+		};
+
+
+	private:
+		float GetHeuristicCost(T_NodeType* pStartNode, T_NodeType* pEndNode) const;
+
+		IGraph<T_NodeType, T_ConnectionType>* m_pGraph;
+		Heuristic m_HeuristicFunction;
+	};
+
+	template <class T_NodeType, class T_ConnectionType>
+	Flowfield<T_NodeType, T_ConnectionType>::Flowfield(IGraph<T_NodeType, T_ConnectionType>* pGraph, Heuristic hFunction)
+		: m_pGraph(pGraph)
+		, m_HeuristicFunction(hFunction)
+	{
+	}
+
+	template <class T_NodeType, class T_ConnectionType>
+	float Flowfield<T_NodeType, T_ConnectionType>::GetHeuristicCost(T_NodeType* pStartNode, T_NodeType* pEndNode) const
+	{
+		Vector2 toDestination = m_pGraph->GetNodePos(pEndNode) - m_pGraph->GetNodePos(pStartNode);
+		return m_HeuristicFunction(abs(toDestination.x), abs(toDestination.y));
+	}
+}
